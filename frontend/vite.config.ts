095c7f35c0832b7356991c0deb9b15db32e405ce
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-07-28 23:05:25 +0530

    Create vite.config.ts

---
+++ frontend/vite.config.ts
@@ -0,0 +1,27 @@
+// frontend/vite.config.ts
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173,
+    watch: {
+      usePolling: true, // Necessary for Docker container volume mapping
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  }
+})
