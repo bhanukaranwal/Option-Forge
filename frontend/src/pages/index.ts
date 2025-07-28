1ec5e09f31aa6876dd1aa6d546e3b3824b977523
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-07-28 23:17:48 +0530

    Create index.ts

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,9 @@
+// frontend/src/pages/index.ts
+
+export { default as DashboardPage } from './DashboardPage';
+export { default as LoginPage } from './LoginPage';
+export { default as RegisterPage } from './RegisterPage';
+export { default as StrategyBuilderPage } from './StrategyBuilderPage';
+export { default as BacktestResultsPage } from './BacktestResultsPage';
+export { default as SettingsPage } from './SettingsPage';
+export { default as NotFoundPage } from './NotFoundPage';
