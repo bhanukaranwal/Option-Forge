47d0bd5cfb08e959e5840a36e6fb2b6b47f980ad
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-07-28 23:09:21 +0530

    Create main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,20 @@
+// frontend/src/main.tsx
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.tsx'
+import { Toaster } from 'react-hot-toast'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster position="top-right" reverseOrder={false} />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
